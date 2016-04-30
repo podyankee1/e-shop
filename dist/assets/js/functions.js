7056408a853b1dcd57b7b2ea2cf3a8ea7aab2c06
podyankee <podyankee@gmail.com>
2016-04-30 05:06:51 +0300

    Popular products section finished.Script equal hights.

--- dist/assets/js/functions.js
+++ dist/assets/js/functions.js
@@ -4,4 +4,15 @@ jQuery(document).ready(function($) {
 				arrows: false,
 				animation: 'vertical'
 			});
+
+			setEqualHeight($(".description"));
+
+			function setEqualHeight(columns) {
+					var tallestcolumn = 0;
+					columns.each( function() {
+						currentHeight = $(this).height();
+						if(currentHeight > tallestcolumn) { tallestcolumn = currentHeight; } } );
+						columns.height(tallestcolumn);
+					}
+
 		});
